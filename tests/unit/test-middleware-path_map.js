723695dccbc3163fae99558dc5bbf9356d28f421
Ken Wronkiewicz <wh-github@wirewd.com>
2014-09-07 12:58:55 -0700

    adding path_map tests.

---
+++ tests/unit/test-middleware-path_map.js
@@ -0,0 +1,39 @@
+var path_map = require('../../lib/middleware/path_map');
+var test = require('tape');
+var async = require('async');
+var sitepath = require ('../../lib/sitepath');
+
+test('middleware path_map', function (t) {
+  
+  t.plan(3);
+
+  var res = {};
+
+  var middleware = path_map();
+  t.deepEqual(typeof middleware, "function");
+
+  async.parallel([
+    function(callback) {
+      var req = {path: '/'};
+      middleware(req, res, function()
+      {
+        t.deepEqual(req.sitepath, new sitepath(['wh']));
+        callback()
+      });
+    },
+    function(callback) {
+      var req = {path: '/wh/'};
+      middleware(req, res, function()
+      {
+        t.deepEqual(req.sitepath, new sitepath(['wh','wh']));
+        callback()
+      });
+    }
+  ], function(err, results)
+  {
+    t.end();
+  })
+    
+  
+
+});
