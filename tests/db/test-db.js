b9b35758d252430064469d3e7317d2378d3325f6
Ken Wronkiewicz <wh@wirewd.com>
2014-09-21 19:08:08 -0700

    upgrading test coverage.

--- tests/db/test-db.js
+++ tests/db/test-db.js
@@ -32,3 +32,80 @@ test.test('db', function (t) {
     t.end();
   });
 });
+
+
+test.test('db open_transaction failure', function (t) {
+  t.plan(2);
+  var conString = 'postgresql://wirehead:rm3test@127.0.0.1/rm3unit';
+  Conf._data.endpoints.postgres = conString;
+  var db = require('../../lib/db');
+  client = {
+    query: function(q, callback) {
+      callback(new Error('ffffff'));
+    }
+  };
+  db.open_transaction(client, undefined, function(err){
+    if (err) {
+      t.pass('errored');
+      t.deepEqual(err.name,'DbError');
+    } else {
+      t.fail('should call an error');
+    }
+    t.end();
+  });
+});
+
+test.test('db commit_transaction failure', function (t) {
+  t.plan(2);
+  var conString = 'postgresql://wirehead:rm3test@127.0.0.1/rm3unit';
+  Conf._data.endpoints.postgres = conString;
+  var db = require('../../lib/db');
+  client = {
+    query: function(q, callback) {
+      callback(new Error('ffffff'));
+    }
+  };
+  db.commit_transaction(client, function(err){
+    if (err) {
+      t.pass('errored');
+      t.deepEqual(err.name,'DbCommitFailedError');
+    } else {
+      t.fail('should call an error');
+    }
+    t.end();
+  });
+});
+
+test.test('db rollback_transaction failure', function (t) {
+  t.plan(2);
+  var conString = 'postgresql://wirehead:rm3test@127.0.0.1/rm3unit';
+  Conf._data.endpoints.postgres = conString;
+  var db = require('../../lib/db');
+  client = {
+    query: function(q, callback) {
+      callback(new Error('ffffff'));
+    }
+  };
+  db.rollback_transaction(client, function(err){
+    if (err) {
+      t.pass('errored');
+      t.deepEqual(err.name,'DbRollbackFailedError');
+    } else {
+      t.fail('should call an error');
+    }
+    t.end();
+  });
+});
+
+test.test('db wrap_error', function (t) {
+  t.plan(2);
+  var conString = 'postgresql://wirehead:rm3test@127.0.0.1/rm3unit';
+  Conf._data.endpoints.postgres = conString;
+  var db = require('../../lib/db');
+  err = db.wrap_error(new Error('could not connect to server: Connection refused'));
+  t.deepEqual(err.name, 'DbConnectionRefusedError');
+  err = db.wrap_error(new Error('relation wh_frro does not exist'));
+  t.deepEqual(err.name, 'DbTableMissingError');
+  t.end();
+
+});
