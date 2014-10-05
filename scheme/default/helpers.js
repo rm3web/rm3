9a5eda6e10e7a3bd6f21583213632e7a094c1aa8
Ken Wronkiewicz <wh@wirewd.com>
2014-10-05 12:37:12 -0700

    UI work.

---
+++ scheme/default/helpers.js
@@ -0,0 +1,27 @@
+
+exports = module.exports = function(dust) {
+    dust.helpers.menu = function (chunk, ctx, bodies, params) {
+        longstr = '<div class="l-box">';
+        longstr = longstr + '<div class="pure-menu pure-menu-open">\
+        <a class="pure-menu-heading">Admin</a>\
+    <ul>';
+        if (ctx.get('section') === 'edit') {
+            longstr = longstr + '<li class="pure-menu-disabled"><a href="#">Edit</a></li>';
+        } else {
+            longstr = longstr + '<li><a href="edit.html">Edit</a></li>';
+        }
+        longstr = longstr + '<li><a href="#">Tag</a></li>\
+    <li><a href="delete.html">Delete</a></li>\
+    <li><a href="#">Move</a></li>\
+    <li><a href="#" data-dropdown="#dropdown-1">Create &#x25BC;</a></li>\
+    <li class="pure-menu-heading">User</li>\
+    <li><a href="#">Log Out</a></li>\
+    </ul>\
+</div><div id="dropdown-1" class="dropdown dropdown-tip">\
+    <ul class="dropdown-menu">\
+        <li><a href="create.html?type=base">Default Node</a></li>\
+    </ul>\
+</div>';
+        return chunk.write(longstr);
+    }
+}
