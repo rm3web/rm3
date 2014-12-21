78225bd9ddf7ac511398086a2a47de2ad9b857ed
Ken Wronkiewicz <wh@wirewd.com>
2014-12-21 12:48:53 -0800

    added a new behavior to entity, improved test coverage.

--- tests/unit/test-entity.js
+++ tests/unit/test-entity.js
@@ -53,6 +53,43 @@ test('entity from_db', function (t) {
   t.end();
 });
 
+test('entity from_db to_rec', function (t) {
+  t.plan(1);
+
+  var e = new entity.Entity();
+  var queryresp = { 
+    command: 'SELECT',
+    rowCount: 1,
+    oid: NaN,
+    rows: 
+    [{  path: 'wh',
+        stub: false,
+        entity_id: '96010990-36ad-11e4-863b-614e8d833a23',
+        revision_id: '96010991-36ad-11e4-863b-614e8d833a23',
+        revision_num: 1,
+        proto: 'base',
+        modified: new Date('Sun Sep 07 2014 09:39:50 GMT-0700 (PDT)'),
+        created: new Date('Sun Sep 07 2014 09:39:50 GMT-0700 (PDT)'),
+        summary: { title: 'blrg', abstract: 'some text goes here' },
+        data: { posting: '<div>Test test</div>' } }  
+    ],
+    fields: 
+    [ { name: 'path', dataTypeID: 17555 },
+      { name: 'stub', dataTypeID: 16 },
+      { name: 'entity_id', dataTypeID: 2950 },
+      { name: 'revision_id', dataTypeID: 2950 },
+      { name: 'revision_num', dataTypeID: 23 },
+      { name: 'proto', dataTypeID: 25 },
+      { name: 'modified', dataTypeID: 1114 },
+      { name: 'created', dataTypeID: 1114 },
+      { name: 'summary', dataTypeID: 114 },
+      { name: 'data', dataTypeID: 114 } ],
+  rowAsArray: false };
+  e.from_db(queryresp);
+  t.deepEqual(queryresp.rows[0],e.to_rec());
+  t.end();
+});
+
 test('entity view', function (t) {
   t.plan(1);
 
@@ -82,6 +119,26 @@ test('entity view', function (t) {
   data: {posting: '<div>Test test</div>' }};
 
   t.deepEqual(e.view(),v);
+  t.end();
+});
+
+test('entity path', function (t) {
+  t.plan(1);
+
+  var e = new entity.Entity();
+  e._path = new sitepath(['wh']);
+  e._entity_id = '96010990-36ad-11e4-863b-614e8d833a23';
+  e._revision_id = '96010991-36ad-11e4-863b-614e8d833a23';
+  e._revision_num = 1;
+  e._proto = "base";
+  e._modified = new Date('Sun Sep 07 2014 09:39:50 GMT-0700 (PDT)');
+  e._created = new Date('Sun Sep 07 2014 09:39:50 GMT-0700 (PDT)');
+  e.data.posting = '<div>Test test</div>';
+  e.summary = 
+  {"title": "blrg",
+   "abstract": "some text goes here"};
+
+  t.deepEqual(e.path(), new sitepath(['wh']));
   t.end();
 });
 
