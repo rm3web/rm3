091cad6485694fbf4e28674a642637dfad1d40e6
Ken Wronkiewicz <wh@wirewd.com>
2015-05-17 10:19:24 -0700

    fix user lint.

--- tests/db/test-user.js
+++ tests/db/test-user.js
@@ -10,18 +10,6 @@ var db = require('../../lib/db');
 var query = require('../../lib/query');
 require('mocha-steps');
 
-function quick_query(db, querytext, next) {
-  async.waterfall([
-    db.connect_wrap,
-    function(client, done, callback){
-      client.query(querytext, function(err, result){
-        done(err);
-        callback(err, result);
-      });
-    }
-  ], next);
-}
-
 describe('user', function() {
   var now = new Date();
   var ents = {};
