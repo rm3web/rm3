ca30127c7ad8313a6742422bae4a9b81222dbd2d
Ken Wronkiewicz <wh-github@wirewd.com>
2015-05-17 11:45:01 -0700

    fixed a bunch of lint rules.

--- tests/unit/test-query.js
+++ tests/unit/test-query.js
@@ -11,47 +11,47 @@ describe('query gen', function() {
 
   describe('throws an error', function() {
     it('throws an error on invalid query select', function() {
-      (function(){
-        query._queryGen({}, 'wh','retr','entity',{},undefined,undefined);
+      (function() {
+        query._queryGen({}, 'wh', 'retr', 'entity', {}, undefined, undefined);
       }).should.throw('invalid query');
     });
     it('throws an error on invalid query target', function() {
-      (function(){
-        query._queryGen(root, 'wh','child','retr',{},undefined,undefined);
+      (function() {
+        query._queryGen(root, 'wh', 'child', 'retr', {}, undefined, undefined);
       }).should.throw('invalid query');
     });
   });
 
   describe('generates the correct queries', function() {
     var tests = [
-      {args: [root, 'wh','child','entity',{},undefined,undefined],
+      {args: [root, 'wh', 'child', 'entity', {}, undefined, undefined],
        expected: 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (wh_entity.path <@ $1) ORDER BY path ASC'},
-      {args: [root, 'wh','child','count',{},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1)'},
-      {args: [root, 'wh','parents','count',{},undefined,undefined],
+      {args: [root, 'wh', 'parents', 'count', {}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path @> $1)'},
-      {args: [root, 'wh','dir','count',{},undefined,undefined],
+      {args: [root, 'wh', 'dir', 'count', {}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path ~ lquery($1 || \'.*{1}\'))'},
-      {args: [root, 'wh','child','count',{},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1)'},
-      {args: [root, 'wh','child','count',{protos: ['blah']},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {protos: ['blah']}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1) AND (proto = $2)'},
-      {args: [root, 'wh','child','count',{notprotos: ['blah']},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {notprotos: ['blah']}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1) AND (proto <> $2)'},
-      {args: [root, 'wh','child','count',{before: 123},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {before: 123}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1) AND (created < $2)'},
-      {args: [root, 'wh','child','count',{after: 123},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {after: 123}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1) AND (created >= $2)'},
-      {args: [root, 'wh','child','count',{navbar: true},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {navbar: true}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity INNER JOIN wh_tag ON (wh_tag."subjPath" = wh_entity.path) WHERE (wh_entity.path <@ $1) AND ("predPath" = \'navigation\') AND ("objStr" = \'navbar\')'},
-      {args: [root, 'wh','child','count',{tag: 'bears'},undefined,undefined],
+      {args: [root, 'wh', 'child', 'count', {tag: 'bears'}, undefined, undefined],
        expected: 'SELECT count(*) FROM wh_entity INNER JOIN wh_tag ON (wh_tag."subjPath" = wh_entity.path) WHERE (wh_entity.path <@ $1) AND ("predPath" = \'plain\') AND ("objStr" = $2)'},
-      {args: [root, 'wh','child','entity',{},'changed',undefined],
+      {args: [root, 'wh', 'child', 'entity', {}, 'changed', undefined],
        expected: 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (wh_entity.path <@ $1) ORDER BY modified ASC'},
-      {args: [root, 'wh','child','entity',{},'created',undefined],
+      {args: [root, 'wh', 'child', 'entity', {}, 'created', undefined],
        expected: 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (wh_entity.path <@ $1) ORDER BY created ASC'}
     ];
-    
+
     tests.forEach(function(test, index) {
       // Need to name this better
       it('correctly builds ' + index, function() {
@@ -63,12 +63,12 @@ describe('query gen', function() {
 });
 
 describe('query', function() {
-  it('#entityFromPath()', function (done) {
+  it('#entityFromPath()', function(done) {
     var plan = new Plan(2, done);
-    
+
     var selectQuery = 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (path = $1)';
-    
-    var entpath = new sitepath(['wh','rq']);
+
+    var entpath = new sitepath(['wh', 'rq']);
 
     var Entclass = function() {};
 
@@ -77,20 +77,19 @@ describe('query', function() {
 
     var db = {};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec, func) {
         should.deepEqual(spec.text, selectQuery);
         func(null, {rowCount: 1});
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
-    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, null, function(err, entity){
-      if(err) {
+    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, null, function(err, entity) {
+      if (err) {
         should.fail(err);
       } else {
       }
@@ -98,29 +97,28 @@ describe('query', function() {
     });
   });
 
-  it('#fetchEffectivePermissions()', function (done) {
+  it('#fetchEffectivePermissions()', function(done) {
     var plan = new Plan(2, done);
     var selectQuery = 'SELECT permission, wh_subject_to_roles.role FROM wh_permission_to_role INNER JOIN wh_subject_to_roles ON (wh_permission_to_role.role = wh_subject_to_roles.role) WHERE (subject = $1) AND (ltree(text($2)) ~ wh_permission_to_role.query)';
-    
+
     var entpath = new sitepath(['wh']);
-    var user = new sitepath(['wh','users','wirehead']);
+    var user = new sitepath(['wh', 'users', 'wirehead']);
 
     var db = {};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec, func) {
         should.deepEqual(spec.text, selectQuery);
         func(null, {rowCount: 1, rows: ['root']});
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
-    query.fetchEffectivePermissions(db, user, entpath, function(err, entity){
-      if(err) {
+    query.fetchEffectivePermissions(db, user, entpath, function(err, entity) {
+      if (err) {
         should.fail(err);
       } else {
       }
@@ -128,11 +126,11 @@ describe('query', function() {
     });
   });
 
-  it('query fromDb not_found', function (done) {
+  it('query fromDb not_found', function(done) {
     var plan = new Plan(2, done);
     var selectQuery = 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (path = $1)';
-    
-    var entpath = new sitepath(['wh','rq']);
+
+    var entpath = new sitepath(['wh', 'rq']);
 
     var Entclass = function() {};
 
@@ -142,22 +140,21 @@ describe('query', function() {
 
     var db = {};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec, func) {
         should.deepEqual(spec.text, selectQuery);
         func(null, {rowCount: 0});
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
-    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, null, function(err, entity){
-      if(err) {
-        should.deepEqual(err.name,'EntityNotFoundError');
-        should.deepEqual(err.path,entpath.toDottedPath());
+    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, null, function(err, entity) {
+      if (err) {
+        should.deepEqual(err.name, 'EntityNotFoundError');
+        should.deepEqual(err.path, entpath.toDottedPath());
       } else {
         should.fail('should not succeed');
       }
@@ -165,11 +162,11 @@ describe('query', function() {
     });
   });
 
-  it('query fromDb error', function (done) {
+  it('query fromDb error', function(done) {
     var plan = new Plan(2, done);
     var selectQuery = 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (path = $1)';
-    
-    var entpath = new sitepath(['wh','rq']);
+
+    var entpath = new sitepath(['wh', 'rq']);
 
     var Entclass = function() {};
 
@@ -179,21 +176,20 @@ describe('query', function() {
 
     var db = {};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec, func) {
         should.deepEqual(spec.text, selectQuery);
         func(new Error("Connection was ended during query"));
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
-    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, null, function(err, entity){
-      if(err) {
-        should.deepEqual(err.name,'QueryError');
+    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, null, function(err, entity) {
+      if (err) {
+        should.deepEqual(err.name, 'QueryError');
       } else {
         should.fail('should not succeed');
       }
@@ -201,13 +197,13 @@ describe('query', function() {
     });
   });
 
-  it('query fromDb not_found log', function (done) {
+  it('query fromDb not_found log', function(done) {
     var plan = new Plan(2, done);
     var selectQuery = 'SELECT path, "entityId", note, "baseRevisionId", "replaceRevisionId", \
 "revisionId", "revisionNum", "evtStart", "evtEnd", "evtTouched", "evtClass", "evtFinal", data \
 FROM wh_log WHERE ("revisionId" = $1)';
 
-    var entpath = new sitepath(['wh','rq']);
+    var entpath = new sitepath(['wh', 'rq']);
 
     var Entclass = function() {};
 
@@ -217,21 +213,20 @@ FROM wh_log WHERE ("revisionId" = $1)';
 
     var db = {};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec, func) {
         should.deepEqual(spec.text, selectQuery);
         func(null, {rowCount: 0});
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
-    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, '1234', function(err, entity){
-      if(err) {
-        should.deepEqual(err.name,'RevisionIdNotFoundError');
+    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, '1234', function(err, entity) {
+      if (err) {
+        should.deepEqual(err.name, 'RevisionIdNotFoundError');
       } else {
         should.fail('should not succeed');
       }
@@ -239,13 +234,13 @@ FROM wh_log WHERE ("revisionId" = $1)';
     });
   });
 
-  it('query fromDb error log', function (done) {
+  it('query fromDb error log', function(done) {
     var plan = new Plan(2, done);
     var selectQuery = 'SELECT path, "entityId", note, "baseRevisionId", "replaceRevisionId", \
 "revisionId", "revisionNum", "evtStart", "evtEnd", "evtTouched", "evtClass", "evtFinal", data \
 FROM wh_log WHERE ("revisionId" = $1)';
-    
-    var entpath = new sitepath(['wh','rq']);
+
+    var entpath = new sitepath(['wh', 'rq']);
 
     var Entclass = function() {};
 
@@ -255,21 +250,20 @@ FROM wh_log WHERE ("revisionId" = $1)';
 
     var db = {};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec, func) {
         should.deepEqual(spec.text, selectQuery);
         func(new Error("Connection was ended during query"));
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
-    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, '1535', function(err, entity){
-      if(err) {
-        should.deepEqual(err.name,'QueryError');
+    query.entityFromPath(db, Entclass, {context: "ROOT"}, entpath, '1535', function(err, entity) {
+      if (err) {
+        should.deepEqual(err.name, 'QueryError');
       } else {
         should.fail('should not succeed');
       }
@@ -277,7 +271,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
     });
   });
 
-  it('query', function (done) {
+  it('query', function(done) {
     var plan = new Plan(3, done);
     var selectQuery = 'SELECT path, stub, hidden, "entityId", "revisionId", "revisionNum", proto, modified, created, touched, summary, data, tags FROM wh_entity WHERE (wh_entity.path <@ $1) ORDER BY path ASC';
 
@@ -285,7 +279,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
 
     var db = {};
 
-    var rec = { path: 'wh.query',
+    var rec = {path: 'wh.query',
       stub: false,
       entityId: 'f5f2e2e0-57cf-11e4-bca4-e7af6fd7ffe4',
       revisionId: 'f5f2e2e1-57cf-11e4-bca4-e7af6fd7ffe4',
@@ -293,10 +287,10 @@ FROM wh_log WHERE ("revisionId" = $1)';
       proto: 'base',
       modified: new Date(),
       created: new Date(),
-      summary: { title: 'blrg', abstract: 'some text goes here' },
-      data: { posting: '<div></div>' } };
+      summary: {title: 'blrg', abstract: 'some text goes here'},
+      data: {posting: '<div></div>'}};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec) {
         should.deepEqual(spec.text, selectQuery);
@@ -307,15 +301,14 @@ FROM wh_log WHERE ("revisionId" = $1)';
         });
         return ee;
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
     var root = {context: "ROOT"};
 
-    var resp = query.query(db, root, entpath, 'child','entity',{},undefined,undefined);
+    var resp = query.query(db, root, entpath, 'child', 'entity', {}, undefined, undefined);
     resp.on('article', function(article) {
       should.deepEqual(article.title, rec.summary.title);
       should.deepEqual(article.summary, rec.summary.abstract);
@@ -330,7 +323,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
     });
   });
 
-  it('query count', function (done) {
+  it('query count', function(done) {
     var plan = new Plan(3, done);
     var selectQuery = 'SELECT count(*) FROM wh_entity WHERE (wh_entity.path <@ $1)';
 
@@ -338,9 +331,9 @@ FROM wh_log WHERE ("revisionId" = $1)';
 
     var db = {};
 
-    var rec = { count: '2' };
+    var rec = {count: '2'};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec) {
         should.deepEqual(spec.text, selectQuery);
@@ -351,15 +344,14 @@ FROM wh_log WHERE ("revisionId" = $1)';
         });
         return ee;
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
 
     var root = {context: "ROOT"};
 
-    var resp = query.query(db, root, entpath, 'child','count',{},undefined,undefined);
+    var resp = query.query(db, root, entpath, 'child', 'count', {}, undefined, undefined);
     resp.on('count', function(article) {
       should.deepEqual(article.count, '2');
       plan.ok(true);
@@ -372,7 +364,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
     });
   });
 
-  it('queryHistory', function (done) {
+  it('queryHistory', function(done) {
     var plan = new Plan(3, done);
     var selectQuery = 'SELECT path, "entityId", note, "baseRevisionId", \
 "replaceRevisionId", "revisionId", "revisionNum", "evtStart", "evtEnd", \
@@ -382,7 +374,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
 
     var db = {};
 
-    var rec = { entityId: 'f5f2e2e0-57cf-11e4-bca4-e7af6fd7ffe4',
+    var rec = {entityId: 'f5f2e2e0-57cf-11e4-bca4-e7af6fd7ffe4',
             note: 'test',
             baseRevisionId: 'f5f2e2e0-57cf-11e4-bca4-e7af6fd7ffe4',
             replaceRevisionId: 'f5f2e2e0-57cf-11e4-bca4-e7af6fd7ffe4',
@@ -395,7 +387,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
             evtFinal: true,
             data: {}};
 
-    db.connectWrap = function (queryfunc) {
+    db.connectWrap = function(queryfunc) {
       var client = {};
       client.query = function(spec) {
         should.deepEqual(spec.text, selectQuery);
@@ -406,8 +398,7 @@ FROM wh_log WHERE ("revisionId" = $1)';
         });
         return ee;
       };
-      queryfunc(null, client, function()
-        {
+      queryfunc(null, client, function() {
         plan.ok(true);
       });
     };
