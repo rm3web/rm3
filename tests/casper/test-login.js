040f305e8448159992ab4296ef8d6e16ca22785b
Ken Wronkiewicz <wh-github@wirewd.com>
2015-05-23 12:16:31 -0700

    added a failed login and an edit to the crud test.

--- tests/casper/test-login.js
+++ tests/casper/test-login.js
@@ -2,12 +2,35 @@
 /*jshint expr:true */
 
 describe('User', function() {
-  before(function() {
+  beforeEach(function() {
     casper.start('http://127.0.0.1:4000/');
   });
 
   after(function() {
     casper.thenOpen('http://127.0.0.1:4000/$logout');
+  });
+
+  it('should reject bad passwords', function() {
+    casper.then(function() {
+      'Welcome to rm3'.should.matchTitle;
+      'a[href*=login]'.should.be.inDOM.and.be.visible;
+      'div.footer'.should.be.inDOM.and.be.visible;
+      this.click('a[href*=login]');
+    });
+
+    casper.then(function() {
+      'form[action*=login]'.should.be.inDOM.and.be.visible;
+      'div.footer'.should.be.inDOM.and.be.visible;
+      this.fill('form[action*=login]',
+        {username: 'wirehead',
+         password: 'pass'}, true);
+    });
+
+    casper.then(function() {
+      'form[action*=login]'.should.be.inDOM.and.be.visible;
+      'div.errormessage'.should.be.inDOM.and.contain.text('Error: Password Validation failed');
+      'div.footer'.should.be.inDOM.and.be.visible;
+    });
   });
 
   it('should be able to log in and out', function() {
