9ec72464b3a762e74499bce4a2ff2b0450d59137
Ken Wronkiewicz <wh-github@wirewd.com>
2015-05-23 18:13:26 -0700

    improving logging for the user / local_auth.  Improved security minorly.

--- tests/unit/test-logging.js
+++ tests/unit/test-logging.js
@@ -15,75 +15,92 @@ errs.register('loggingmock', LoggingMockError);
 describe('logging', function() {
   var boundLogger, plan;
 
-  beforeEach(function() {
-    boundLogger = {};
-    boundLogger.error = function(message, data){
-      message.should.equal('logging test');
-      data.should.have.property('data','data');
-      plan.ok(true);
-    };
-  });
-
-  context('callback errors', function() {
-    it('#logAndCreateError creates an error', function(cb) {
-      plan = new Plan(2, cb);
-
-      logging.logAndCreateError(boundLogger,'logging test','loggingmock',
-        {data: 'data'}, function(err){
-          err.should.have.property('data','data');
-          err.should.be.an.instanceOf(LoggingMockError);
-          plan.ok(true);
-        });
+  context('triggered by a callback', function() {
+    beforeEach(function() {
+      boundLogger = {};
+      boundLogger.error = function(message, data){
+        message.should.equal('logging test');
+        data.should.have.property('data','data');
+        plan.ok(true);
+      };
     });
 
-    it('#logAndCreateError creates an error', function(cb) {
-      plan = new Plan(2, cb);
-      var err = new Error();
-      err.fear = 'beer';
+    context('sending to a callback', function() {
+      it('#logAndCreateError creates an error', function(cb) {
+        plan = new Plan(2, cb);
 
-      logging.logAndWrapError(boundLogger, err, 'logging test','loggingmock',
-        {data: 'data'}, function(err){
+        logging.logAndCreateError(boundLogger,'logging test','loggingmock',
+          {data: 'data'}, function(err){
+            err.should.have.property('data','data');
+            err.should.be.an.instanceOf(LoggingMockError);
+            plan.ok(true);
+          });
+      });
+
+      it('#logAndCreateError creates an error', function(cb) {
+        plan = new Plan(2, cb);
+        var err = new Error();
+        err.fear = 'beer';
+
+        logging.logAndWrapError(boundLogger, err, 'logging test','loggingmock',
+          {data: 'data'}, function(err){
+            err.should.have.property('data','data');
+            err.should.have.property('fear','beer');
+            err.should.have.property('stacktrace');
+            err.should.be.an.instanceOf(LoggingMockError);
+            plan.ok(true);
+          });
+      });
+
+    });
+
+    context('emitting errors', function() {
+      var ee;
+      beforeEach(function() {
+        ee = new events.EventEmitter();
+        ee.on('error', function(err){
           err.should.have.property('data','data');
+          err.should.be.an.instanceOf(LoggingMockError);
+        });
+      });
+      it('#logAndEmitError creates an error', function(cb) {
+        plan = new Plan(2, cb);
+        ee.on('error', function(err){
+          plan.ok(true);
+        });
+
+        logging.logAndEmitError(boundLogger,'logging test','loggingmock',
+          {data: 'data'}, ee);
+      });
+
+      it('#logAndEmitWrapError creates an error', function(cb) {
+        plan = new Plan(2, cb);
+        var err = new Error();
+        err.fear = 'beer';
+        ee.on('error', function(err){
           err.should.have.property('fear','beer');
           err.should.have.property('stacktrace');
-          err.should.be.an.instanceOf(LoggingMockError);
           plan.ok(true);
         });
+        
+        logging.logAndEmitWrapError(boundLogger, err, 'logging test','loggingmock',
+          {data: 'data'}, ee);
+      });
     });
-
   });
 
-  context('emitted errors', function() {
-    var ee;
-    beforeEach(function() {
-      ee = new events.EventEmitter();
-      ee.on('error', function(err){
-        err.should.have.property('data','data');
-        err.should.be.an.instanceOf(LoggingMockError);
-      });
-    });
-    it('#logAndEmitError creates an error', function(cb) {
-      plan = new Plan(2, cb);
-      ee.on('error', function(err){
-        plan.ok(true);
-      });
+  context('emitted events', function() {
+    it('#logEventEmitterErrors logs errors', function(cb) {
+      boundLogger = {};
+      boundLogger.error = function(message, data){
+        message.should.equal('logging test');
+        data.should.have.property('ctx');
+        cb();
+      };
 
-      logging.logAndEmitError(boundLogger,'logging test','loggingmock',
-        {data: 'data'}, ee);
-    });
-
-    it('#logAndEmitWrapError creates an error', function(cb) {
-      plan = new Plan(2, cb);
-      var err = new Error();
-      err.fear = 'beer';
-      ee.on('error', function(err){
-        err.should.have.property('fear','beer');
-        err.should.have.property('stacktrace');
-        plan.ok(true);
-      });
-      
-      logging.logAndEmitWrapError(boundLogger, err, 'logging test','loggingmock',
-        {data: 'data'}, ee);
+      var ee = new events.EventEmitter();
+      logging.logEventEmitterErrors(boundLogger,ee,{data: 'data'},'logging test');
+      ee.emit('error', errs.create('loggingmock', {data:'data'}));
     });
   });
 });
