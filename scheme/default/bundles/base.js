ee449e1a7a7b67c7553068cf232f1138af875763
Ken Wronkiewicz <wh-github@wirewd.com>
2015-06-06 21:39:32 -0700

    added test, fixed some accidental bugs and lint.

--- scheme/default/bundles/base.js
+++ scheme/default/bundles/base.js
@@ -5,9 +5,8 @@ var textblockUi = require('../../../lib/textblock_ui')
 function convertSoloToSection(prefix) {
   var source = $("textarea[name='" + prefix + "\[source\]']").val();
   var format = $("select[name='" + prefix + "\[format\]']").val();
-  var fieldprefix = prefix + '[blocks]'
-  var block = textblockUi.generateArea(prefix + '[0][source]', source);
-  var format = textblockUi.generateSelect(prefix + '[0][format]', format);
+  var block = textblockUi.generateArea(prefix + '[blocks][0][source]', source);
+  var format = textblockUi.generateSelect(prefix + '[blocks][0][format]', format);
 
   var hidden = '<input type="hidden" value="section" name="posting[format]" /> \
   <input type="hidden" value="1" name="numblocks" />'
