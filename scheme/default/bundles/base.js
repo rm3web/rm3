798d8c03c007de61bbcac0ae12a5814fe25a2799
Ken Wronkiewicz <wh@wirewd.com>
2015-06-06 19:15:22 -0700

    refactored for cleanlieness.

--- scheme/default/bundles/base.js
+++ scheme/default/bundles/base.js
@@ -2,14 +2,6 @@ var $ = require('jquery-browserify');
 
 var textblockUi = require('../../../lib/textblock_ui')
 
-function textblock_create(prefix, num, source, format) {
-
-  var block = textblockUi.generateArea(prefix + '[blocks][' + num + '][source]', source);
-  var format = textblockUi.generateSelect(prefix + '[blocks][' + num + '][format]', format);
-
-  return (block + format);
-}
-
 function convertSoloToSection(prefix) {
   var source = $("textarea[name='" + prefix + "\[source\]']").val();
   var format = $("select[name='" + prefix + "\[format\]']").val();
@@ -21,14 +13,14 @@ function convertSoloToSection(prefix) {
   <input type="hidden" value="1" name="numblocks" />'
   
   $("#textblocks").replaceWith('<fieldset id="textblocks">' +
-    hidden + block + format + textblock_create('posting', 1, '', 'html') + '</fieldset>');
+    hidden + block + format + textblockUi.generateChildBlock('posting', 1, '', 'html') + '</fieldset>');
 }
 
 $( "#addText" ).on( "click", function( event ) {
   if ($("input[name='posting\[format\]']").val() === 'section') {
     var num = parseInt($("input[name='numblocks']").val(),10) + 1;
     $("input[name='numblocks']").val(num)
-    $("#textblocks").append( textblock_create('posting', num, '', 'html') );
+    $("#textblocks").append( textblockUi.generateChildBlock('posting', num, '', 'html') );
   } else {
     convertSoloToSection('posting');
   }
