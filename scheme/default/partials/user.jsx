beec82b7d62648f54bb3a019c33d5eb6b1d8dc5a
Ken Wronkiewicz <wh@wirewd.com>
2015-06-14 13:21:46 -0700

    fixed up isomorphic user page.

--- scheme/default/partials/user.jsx
+++ scheme/default/partials/user.jsx
@@ -5,34 +5,7 @@ var IntlMixin  = ReactIntl.IntlMixin;
 var FormattedMessage  = ReactIntl.FormattedMessage;
 var forms = require('newforms');
 var i10n = require('../../../lib/i10n');
-
-var UserForm = forms.Form.extend({
-  username: forms.CharField({required: true,
-    widgetAttrs: {className: 'pure-input-1'},
-    label:  i10n.formatMessage('USERNAME', {})}),
-  fullname: forms.CharField({required: true,
-    widgetAttrs: {className: 'pure-input-1'},
-    label: i10n.formatMessage('FULL_NAME', {})}),
-  password: forms.CharField({required: false, 
-    label: i10n.formatMessage('PASSWORD', {}),
-    widgetAttrs: {className: 'pure-input-1'},
-    widget: forms.PasswordInput}),
-  confirmPassword: forms.CharField({required: false, 
-    label: i10n.formatMessage('CONFIRM_PASSWORD', {}),
-    widgetAttrs: {className: 'pure-input-1'},
-    widget: forms.PasswordInput}),
-  profileUrl: forms.URLField({required: false,
-    widgetAttrs: {className: 'pure-input-1'},
-    label: i10n.formatMessage('PROFILE_URL', {})}),
-  email: forms.EmailField({required: false,
-    widgetAttrs: {className: 'pure-input-1'},
-    label: i10n.formatMessage('EMAIL', {})}),
-  profileText: forms.CharField({
-    required: false, 
-    widgetAttrs: {className: 'pure-input-1'},
-    label: i10n.formatMessage('PROFILE_TEXT', {}),
-    widget: forms.Textarea})
-});
+var UserForm = require('../../../lib/forms/user.js').UserForm;
 
 var Username =  React.createClass({
   render: function() {
@@ -44,14 +17,14 @@ var Username =  React.createClass({
   }
 });
 
-var LoginFormComponent = React.createClass({
+var UserFormComponent = React.createClass({
   mixins: [IntlMixin],
   render: function() {
     var buttonMessage = 'submit';
-    var action = 'create.html?type={meta.proto}'
+    var action = 'create.html?type=' + this.props.proto;
     var data;
 
-    if (this.props.hasOwnProperty('username')) {
+    if (this.props.hasOwnProperty('fullname')) {
       data = {data: {
         username: this.props.username,
         fullname: this.props.fullname,
@@ -64,17 +37,19 @@ var LoginFormComponent = React.createClass({
     var form = new UserForm(data);
     var username;
 
-    if (!this.props.hasOwnProperty('username') && form.isInitialRender) {
-      form.validate(document.getElementById('userform-form'));
-      username = document.getElementById('userform-username').innerHTML;
-    } else {
-      username = this.props.username;
-    }
-
     if (this.props.section === 'edit') {
+      if (!this.props.hasOwnProperty('username') && form.isInitialRender) {
+        form.validate(document.getElementById('userform-form'));
+        username = document.getElementById('userform-username').innerHTML;
+      } else {
+        username = this.props.username;
+      }
       delete form.fields.username;
       buttonMessage = 'edit';
       action = 'edit.html'
+    } else {
+      form.fields.password.required = true;
+      form.fields.confirmPassword.required = true;
     }
 
     return (<form action={action} id="userform-form" method="post" className="pure-form pure-form-stacked" onSubmit={this.onSubmit}>
@@ -83,14 +58,7 @@ var LoginFormComponent = React.createClass({
       <button> <FormattedMessage
                     message={buttonMessage} /></button>
     </form>);
-  },
-  clean: function() {
-    if (this.cleanedData.password &&
-        this.cleanedData.confirmPassword &&
-        this.cleanedData.password != this.cleanedData.confirmPassword) {
-      throw forms.ValidationError('Passwords do not match.')
-    }
   }
 });
 
-module.exports = LoginFormComponent;
+module.exports = UserFormComponent;
