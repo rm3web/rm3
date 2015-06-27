df0d38778a6b9132f39345bf3e8bac668f1fa0d8
Ken Wronkiewicz <wh@wirewd.com>
2015-06-27 13:44:41 -0700

    new-style error messages for the form.

--- scheme/default/partials/user.jsx
+++ scheme/default/partials/user.jsx
@@ -14,6 +14,30 @@ var Username =  React.createClass({
   }
 });
 
+var SingleError = React.createClass({
+  render: function() {
+    return (<li>
+      {this.props.error}
+      </li>);
+  }
+});
+
+var ErrorsList = React.createClass({
+  mixins: [IntlMixin],
+  render: function() {
+    console.log(this.props.errors)
+    if (this.props.errors) {
+      return (<div><ul>
+      {this.props.errors.map(function(error, i){
+          return (<SingleError key={i} error={error} />);
+      })}
+      </ul></div>);
+    } else {
+      return <div />
+    }
+  }
+});
+
 var UserFormComponent = React.createClass({
   mixins: [IntlMixin],
   render: function() {
@@ -25,31 +49,42 @@ var UserFormComponent = React.createClass({
       action = 'edit.html'
     }
 
+    var errors = {};
+    if (this.props.errors) {
+      errors = this.props.errors;
+    }
+
     return (<form action={action} id="userform-form" method="post" className="pure-form pure-form-stacked" onSubmit={this.onSubmit}>
       <fieldset>
         <label htmlFor="username" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('LOGIN_ASCII_TEXT_NOSPACES')} />:</label>
         <Username section={this.props.section} username={this.props.username} />
+        <ErrorsList errors={errors.username} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="fullname" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('FULL_NAME')} />:</label>
         <input className="pure-input-1" type="text" name="fullname" placeholder={this.getIntlMessage('FULL_NAME')} value={this.props.fullname} />
+        <ErrorsList errors={errors.fullname} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="password" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PASSWORD_ENTER_TWICE')} />:</label>
         <input className="pure-input-1" type="password" value="" name="password1" placeholder={this.getIntlMessage('PASSWORD')} />
+        <ErrorsList errors={errors.password1} />
         <input className="pure-input-1" type="password" value="" name="password2" placeholder={this.getIntlMessage('CONFIRM_PASSWORD')} />
+        <ErrorsList errors={errors.password2} />
       </fieldset>
 
       <fieldset>
-        <label htmlFor="profile_url" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_URL')} />:</label>
+        <label htmlFor="profileUrl" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_URL')} />:</label>
         <input className="pure-input-1" type="text" id="profileUrl" name="profileUrl" placeholder={this.getIntlMessage("URL")} value={this.props.profileUrl} />
+        <ErrorsList errors={errors.profileUrl} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="email" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('EMAIL')} />:</label>
         <input className="pure-input-1" type="text" id="email" name="email" placeholder={this.getIntlMessage("EMAIL")} value={this.props.email} />
+        <ErrorsList errors={errors.email} />
       </fieldset>
 
       <fieldset>
@@ -57,8 +92,10 @@ var UserFormComponent = React.createClass({
         <textarea rows="5" className="pure-input-1" name="abstract" placeholder={this.getIntlMessage("PROFILE_TEXT")} >
         {this.props.profileText}
         </textarea>
+        <ErrorsList errors={errors.profileText} />
       </fieldset>
 
+      <ErrorsList errors={errors.__all__} />
       <button> <FormattedMessage
                     message={buttonMessage} /></button>
     </form>);
