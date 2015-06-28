1ef5958bbc7a1d7e17e2c7a1b2035faa3754f65c
Ken Wronkiewicz <wh-github@wirewd.com>
2015-06-27 19:48:20 -0700

    user pages should be functional again.

--- scheme/default/partials/user.jsx
+++ scheme/default/partials/user.jsx
@@ -9,7 +9,7 @@ var Username =  React.createClass({
     if (this.props.section === 'edit') {
       return <input id="username" className="pure-input-1" type="text" name="username" value={this.props.username} disabled="true"/>
     } else {
-      return <input id="username" className="pure-input-1" type="text" name="username" placeholder={this.getIntlMessage('USERNAME')} value={this.props.username} />
+      return <input id="username" className="pure-input-1" type="text" name="username" placeholder={this.getIntlMessage('USERNAME')} defaultValue={this.props.username} />
     }
   }
 });
@@ -42,6 +42,7 @@ var UserFormComponent = React.createClass({
   render: function() {
     var buttonMessage = 'submit';
     var action = 'create.html?type=' + this.props.proto;
+    var self = this;
 
     if (this.props.section === 'edit') {
       buttonMessage = 'edit';
@@ -51,6 +52,15 @@ var UserFormComponent = React.createClass({
     var errors = {};
     if (this.props.errors) {
       errors = this.props.errors;
+    }
+    var body = {};
+    if (this.props.body) {
+      body = this.props.body;
+      ['username', 'fullname', 'profileUrl', 'abstract'].forEach(function(field) {
+        if (!self.props[field] && body.hasOwnProperty(field)) {
+          self.props[field] = body[field];
+        }
+      });
     }
 
     return (<form action={action} id="userform-form" method="post" className="pure-form pure-form-stacked" onSubmit={this.onSubmit}>
@@ -62,34 +72,33 @@ var UserFormComponent = React.createClass({
 
       <fieldset>
         <label htmlFor="fullname" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('FULL_NAME')} />:</label>
-        <input className="pure-input-1" type="text" name="fullname" placeholder={this.getIntlMessage('FULL_NAME')} value={this.props.fullname} />
+        <input className="pure-input-1" type="text" name="fullname" placeholder={this.getIntlMessage('FULL_NAME')} defaultValue={this.props.fullname} />
         <ErrorsList errors={errors.fullname} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="password" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PASSWORD_ENTER_TWICE')} />:</label>
-        <input className="pure-input-1" type="password" value="" name="password1" placeholder={this.getIntlMessage('PASSWORD')} />
+        <input className="pure-input-1" type="password" defaultValue="" name="password1" placeholder={this.getIntlMessage('PASSWORD')} />
         <ErrorsList errors={errors.password1} />
-        <input className="pure-input-1" type="password" value="" name="password2" placeholder={this.getIntlMessage('CONFIRM_PASSWORD')} />
+        <input className="pure-input-1" type="password" defaultValue="" name="password2" placeholder={this.getIntlMessage('CONFIRM_PASSWORD')} />
         <ErrorsList errors={errors.password2} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="profileUrl" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_URL')} />:</label>
-        <input className="pure-input-1" type="text" id="profileUrl" name="profileUrl" placeholder={this.getIntlMessage("URL")} value={this.props.profileUrl} />
+        <input className="pure-input-1" type="text" id="profileUrl" name="profileUrl" placeholder={this.getIntlMessage("URL")} defaultValue={this.props.profileUrl} />
         <ErrorsList errors={errors.profileUrl} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="email" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('EMAIL')} />:</label>
-        <input className="pure-input-1" type="text" id="email" name="email" placeholder={this.getIntlMessage("EMAIL")} value={this.props.email} />
+        <input className="pure-input-1" type="text" id="email" name="email" placeholder={this.getIntlMessage("EMAIL")} defaultValue={this.props.email} />
         <ErrorsList errors={errors.email} />
       </fieldset>
 
       <fieldset>
-        <label htmlFor="Profile text" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_TEXT')} />:</label>
-        <textarea rows="5" className="pure-input-1" name="abstract" placeholder={this.getIntlMessage("PROFILE_TEXT")} >
-        {this.props.profileText}
+        <label htmlFor="abstract" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_TEXT')} />:</label>
+        <textarea defaultValue={this.props.profileText} rows="5" className="pure-input-1" name="abstract" placeholder={this.getIntlMessage("PROFILE_TEXT")} >
         </textarea>
         <ErrorsList errors={errors.profileText} />
       </fieldset>
