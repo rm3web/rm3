acb0bd3e5775b3a28ecd25620aa9d2f4d929740d
Ken Wronkiewicz <wh-github@wirewd.com>
2015-07-03 13:54:50 -0700

    client-render for the forms.

--- scheme/default/bundles/user.js
+++ scheme/default/bundles/user.js
@@ -1,1 +1,27 @@
 var React = require('react');
+var UserFormComponent = require('../partials/user.jsx');
+
+var renderTarget = document.getElementById('userform');
+var UserFactory = React.createFactory(UserFormComponent);
+
+var username = document.getElementById('username').value;
+var fullname = document.getElementById('fullname').value;
+var profileUrl = document.getElementById('profileUrl').value;
+var email = document.getElementById('email').value;
+var profileText = document.getElementById('profileText').innerHTML;
+
+var renderedComponent = React.render(
+  UserFactory({
+    locales: intl.locales,
+    messages: intl.messages,
+    section: section,
+    proto: 'user',
+    username: username,
+    fullname: fullname,
+    profileUrl: profileUrl,
+    email: email,
+    profileText: profileText,
+    body: {}
+  }),
+  renderTarget
+);
