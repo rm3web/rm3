daeee4ecfb14474e15ac34e56426a919b7d1a513
Ken Wronkiewicz <wh@wirewd.com>
2015-07-11 13:48:53 -0700

    client-side validation.

--- scheme/default/partials/user.jsx
+++ scheme/default/partials/user.jsx
@@ -40,6 +40,35 @@ var ErrorsList = React.createClass({
 
 var UserFormComponent = React.createClass({
   mixins: [IntlMixin],
+
+  getInitialState: function() {
+    if (this.props.errors) {
+      return {errors: this.props.errors};
+    } else {
+      return {errors: {}};
+    }
+  },
+
+  onSubmit: function (event) {
+    var self = this;
+    event.preventDefault();
+    var userForm = new UserForm(this.props.section === 'edit');
+    var body = {};
+    ['username', 'fullname', 'email', 'password1', 'password2', 'profileUrl', 
+    'profileText'].forEach(function(field) {
+      var val = document.getElementById(field)
+      if (val) {
+        body[field] = val.value;
+      }
+    })
+    userForm.checkForm(body, function(err) {
+      if (err) {
+        self.setState({errors: err});
+      } else {
+        document.forms["userform-form"].submit();
+      }
+    });
+  },
   render: function() {
     var buttonMessage = 'submit';
     var action = 'create.html?type=' + this.props.proto;
@@ -50,62 +79,58 @@ var UserFormComponent = React.createClass({
       action = 'edit.html'
     }
 
-    var errors = {};
-    if (this.props.errors) {
-      errors = this.props.errors;
-    }
     var body = {};
     if (this.props.body) {
       body = this.props.body;
-      ['username', 'fullname', 'email', 'profileUrl', 'abstract'].forEach(function(field) {
+      ['username', 'fullname', 'email', 'profileUrl', 'profileText'].forEach(function(field) {
         if (!self.props[field] && body.hasOwnProperty(field)) {
           self.props[field] = body[field];
         }
       });
     }
-
+    
     return (<form action={action} id="userform-form" method="post" className="pure-form pure-form-stacked" onSubmit={this.onSubmit}>
       <fieldset>
         <label htmlFor="username" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('LOGIN_ASCII_TEXT_NOSPACES')} />:</label>
         <Username section={this.props.section} username={this.props.username} />
-        <ErrorsList errors={errors.username} />
+        <ErrorsList errors={this.state.errors.username} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="fullname" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('FULL_NAME')} />:</label>
         <input className="pure-input-1" id="fullname" type="text" name="fullname" placeholder={this.getIntlMessage('FULL_NAME')} defaultValue={this.props.fullname} />
-        <ErrorsList errors={errors.fullname} />
+        <ErrorsList errors={this.state.errors.fullname} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="password" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PASSWORD_ENTER_TWICE')} />:</label>
-        <input className="pure-input-1" type="password" defaultValue="" name="password1" placeholder={this.getIntlMessage('PASSWORD')} />
-        <ErrorsList errors={errors.password1} />
-        <input className="pure-input-1" type="password" defaultValue="" name="password2" placeholder={this.getIntlMessage('CONFIRM_PASSWORD')} />
-        <ErrorsList errors={errors.password2} />
+        <input className="pure-input-1" type="password" defaultValue="" id="password1" name="password1" placeholder={this.getIntlMessage('PASSWORD')} />
+        <ErrorsList errors={this.state.errors.password1} />
+        <input className="pure-input-1" type="password" defaultValue="" id="password2" name="password2" placeholder={this.getIntlMessage('CONFIRM_PASSWORD')} />
+        <ErrorsList errors={this.state.errors.password2} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="profileUrl" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_URL')} />:</label>
         <input className="pure-input-1" type="text" id="profileUrl" name="profileUrl" placeholder={this.getIntlMessage("URL")} defaultValue={this.props.profileUrl} />
-        <ErrorsList errors={errors.profileUrl} />
+        <ErrorsList errors={this.state.errors.profileUrl} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="email" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('EMAIL')} />:</label>
         <input className="pure-input-1" type="text" id="email" name="email" placeholder={this.getIntlMessage("EMAIL")} defaultValue={this.props.email} />
-        <ErrorsList errors={errors.email} />
+        <ErrorsList errors={this.state.errors.email} />
       </fieldset>
 
       <fieldset>
         <label htmlFor="profileText" className="pure-input-1"><FormattedMessage message={this.getIntlMessage('PROFILE_TEXT')} />:</label>
         <textarea defaultValue={this.props.profileText} rows="5" className="pure-input-1" id="profileText" name="profileText" placeholder={this.getIntlMessage("PROFILE_TEXT")} >
         </textarea>
-        <ErrorsList errors={errors.profileText} />
+        <ErrorsList errors={this.state.errors.profileText} />
       </fieldset>
 
-      <ErrorsList errors={errors.__all__} />
-      <input type="submit" value="submit" />
+      <ErrorsList errors={this.state.errors.__all__} />
+      <button type="submit"><FormattedMessage message={buttonMessage} /></button>
     </form>);
   }
 });
