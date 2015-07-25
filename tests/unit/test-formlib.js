c978bea8a98b7eceb3fe3b4398c98ce8fc28a7d5
Ken Wronkiewicz <wh-github@wirewd.com>
2015-07-25 11:14:42 -0700

    formlib has unit tests.

--- tests/unit/test-formlib.js
+++ tests/unit/test-formlib.js
@@ -1,33 +1,33 @@
 var FormLib = require ('../../lib/formlib');
 var should = require('should');
 
-/*
-
-exports.markError = function markError(error, element, msg) {
-  if (!error.hasOwnProperty(element)) {
-    error[element] = [];
-  }
-  error[element].push(msg);
-};
-
-exports.checkFieldsPresent = function checkFieldsPresent(body, fields, error) {
-  fields.forEach(function(element, index, array) {
-    if (validator.isNull(body[element])) {
-      exports.markError(error, element, {error: 'missing'});
-    }
-  });
-};
-
-exports.nullOrValid = function nullOrValid(body, element, valid, msg, error) {
-  if (!validator.isNull(body[element])) {
-    if (!valid(body[element])) {
-      exports.markError(error, element, msg);
-    }
-  }
-};
-
-*/
 describe('formlib', function() {
+  it('should #checkFieldsPresent', function() {
+    var error = {};
+    var body = {'butter' : 'buttereater', 'moo' : ''};
+    var fields = ['goo', 'butter', 'moo'];
+    FormLib.checkFieldsPresent(body, fields, error);
+    error.should.have.property('goo').with.lengthOf(1);
+    error.goo[0].should.have.property('error');
+    error.goo[0].error.should.equal('missing');
+    error.should.have.property('moo').with.lengthOf(1);
+    error.moo[0].should.have.property('error');
+    error.moo[0].error.should.equal('missing');
+  });
+
+  it('should check #nullOrValid', function() {
+    var body = {'buttereater': true, 'strawberries': false};
+    var valid = function(value) {
+      return value;
+    };
+    var error = {};
+    FormLib.nullOrValid(body, 'buttereater', valid, 'b', error);
+    error.should.not.have.property('buttereater');
+    FormLib.nullOrValid(body, 'strawberries', valid, 'b', error);
+    error.should.have.property('strawberries').with.lengthOf(1);
+    error.strawberries[0].should.equal('b');
+  });
+
   it('should mark an error', function() {
     var error = {};
     FormLib.markError(error, 'element', 'msg');
