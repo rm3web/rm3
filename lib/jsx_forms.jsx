068878ce031c6ae24619995c089e24b61b43c372
Ken Wronkiewicz <wh@wirewd.com>
2015-07-25 17:14:13 -0700

    added jsx form library.

---
+++ lib/jsx_forms.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactIntl = require('react-intl');
+var IntlMixin  = ReactIntl.IntlMixin;
+
+var SingleError = React.createClass({
+  render: function() {
+    return (<li>
+      {this.props.error}
+      </li>);
+  }
+});
+
+var ErrorsList = React.createClass({
+  mixins: [IntlMixin],
+  render: function() {
+    if (this.props.errors) {
+      return (<div><ul>
+      {this.props.errors.map(function(error, i){
+          return (<SingleError key={i} error={error} />);
+      })}
+      </ul></div>);
+    } else {
+      return <div />;
+    }
+  }
+});
+
+exports.SingleError = SingleError;
+exports.ErrorsList = ErrorsList;
