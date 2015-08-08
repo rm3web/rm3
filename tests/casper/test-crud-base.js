0e81e4fbaef01fee323f7dcc09e8c29bb37ed0ea
Ken Wronkiewicz <wh-github@wirewd.com>
2015-08-08 15:40:16 -0700

    fully disabled logging of errors.

--- tests/casper/test-crud-base.js
+++ tests/casper/test-crud-base.js
@@ -7,11 +7,11 @@ describe('Base type CRUD', function() {
     //casper.options.verbose = true;
     casper.start('http://127.0.0.1:4000/');
 
+/*
     casper.on('page.error', function(msg, trace) {
       this.echo('Error: ' + msg, 'ERROR');
     });
 
-/*
     casper.on('remote.message', function(msg) {
       this.echo('Console: ' + msg, 'ERROR');
     });
