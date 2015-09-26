5c878f877e8900163a0927db37b6e3385031df3d
Ken Wronkiewicz <wh-github@wirewd.com>
2015-09-26 10:56:57 -0700

    Giant UI refactor * Removed jquery and jquery dropdown * UI refactoring. * Added top menu bar for logged in users. * Refactored CSS handling with CSS minification * Moved PureCSS off of the CDN and into a build process

--- tests/unit/test-taghelpers.js
+++ tests/unit/test-taghelpers.js
@@ -42,7 +42,11 @@ describe('taghelpers', function() {
 
   describe('#objLink', function() {
     it('works for plain tags', function(cb) {
-      var chunk = {}, context = {}, params = {predClass: 'tag', predKey: 'plain', objKey: 'woof'};
+      var chunk = {}, context = {}
+      var params = {predClass: 'tag', predKey: 'plain', objKey: 'woof'};
+      context.resolve = function(param) {
+        return param;
+      }
       chunk.write = function(str) {
         str.should.equal('<a href="/tags.html/$/woof">woof</a>');
         cb();
@@ -55,7 +59,11 @@ describe('taghelpers', function() {
     });
 
     it('works for predicates', function(cb) {
-      var chunk = {}, context = {}, params = {predClass: 'boof', predKey: 'foof', objKey: 'woof'};
+      var chunk = {}, context = {}
+      var params = {predClass: 'boof', predKey: 'foof', objKey: 'woof'};
+      context.resolve = function(param) {
+        return param;
+      }
       chunk.write = function(str) {
         str.should.equal('woof');
         cb();
