ad0bdf4c2224723f1972e91d9919e11714601a10
Ken Wronkiewicz <wh-github@wirewd.com>
2016-03-06 17:43:16 -0800

    even more refactoring of the gulpfile.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,27 +2,12 @@
 var gulp = require('gulp')
   , nodemon = require('gulp-nodemon')
   , shell = require('gulp-shell')
-  , jscs = require('gulp-jsxcs')
-  , jshint = require('gulp-jshint')
-  , gutil = require('gulp-util')
   , bower = require('gulp-bower')
-  , spawn = require('child_process').spawn
-  , clone = require('clone')
-  , source = require('vinyl-source-stream')
-  , glob = require('glob')
-  , imagemin = require('gulp-imagemin')
-  , rsvg_convert = require('gulp-rsvg')
-  , browserify = require('browserify')
-  , async = require('async')
-  , path = require('path')
-  , rename = require("gulp-rename")
-  , csslint = require('gulp-csslint')
-  , concat = require('gulp-concat')
-  , minifyCss = require('gulp-minify-css')
   ;
 
 require('./tests.js');
 require('./lint.js');
+require('./bundle.js');
 
   // Load and use polyfill for ECMA-402.
 if (!global.Intl) {
@@ -32,56 +17,6 @@ if (!global.Intl) {
 var winston = require('winston');
 
 winston.remove(winston.transports.Console);
-
-gulp.task('icon-75', function() {
-    return gulp.src('./scheme/default/images/icon-*.svg')
-        .pipe(rsvg_convert({width: 75, height:75}))
-        .pipe(rename(function (path) {
-          path.basename += "-75";
-        }))
-        .pipe(gulp.dest('./scheme/default/static/images/'));
-});
-
-gulp.task('icon-24', function() {
-    return gulp.src('./scheme/default/images/icon-*.svg')
-        .pipe(rsvg_convert({width: 24, height:24}))
-        .pipe(rename(function (path) {
-          path.basename += "-24";
-        }))
-        .pipe(gulp.dest('./scheme/default/static/images/'));
-});
-
-gulp.task('cssbundle', function() {
-  return gulp.src(['./bower_components/pure/pure.css',
-    './node_modules/react-super-select/lib/react-super-select.css',
-    './scheme/default/styles/*.css'])
-    .pipe(concat('bundle.css'))
-    .pipe(minifyCss())
-    .pipe(gulp.dest('./scheme/default/static/'));
-})
-
-gulp.task('icon', ['icon-75', 'icon-24'], function() {
-
-})
-
-gulp.task('imagemin', function () { 
-    return gulp.src('./scheme/default/images/*.svg')
-        .pipe(imagemin({}))
-        .pipe(gulp.dest('./scheme/default/static/images/'));
-});
- 
-gulp.task('browserify', function(cb) {
-  var files = glob.sync('./scheme/default/bundles/*.js', {});
-  async.each(files, function(file, next){
-    return browserify(file)
-      .bundle()
-      //Pass desired output filename to vinyl-source-stream
-      .pipe(source(path.basename(file)))
-      // Start piping stream to tasks!
-      .pipe(gulp.dest('./scheme/default/static/bundles/'))
-      .on('end', next);
-  }, cb)
-});
 
 gulp.task('coveralls', shell.task([
   'cat ./coverage/lcov.info |  ./node_modules/codecov.io/bin/codecov.io.js'
