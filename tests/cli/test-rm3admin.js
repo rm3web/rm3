c9721c4e63a11e97989d046c699024c09a50b0bc
Ken Wronkiewicz <wh-github@wirewd.com>
2016-03-12 12:21:31 -0800

    adding CLI tests and sped up the existing tests.

---
+++ tests/cli/test-rm3admin.js
@@ -0,0 +1,43 @@
+var Conf = require ('../../lib/conf');
+var should = require('should');
+var childProcess = require('child_process');
+
+var db = require('../../lib/db');
+var entity = require('../../lib/entity');
+var sitepath = require ('sitepath');
+var query = require('../../lib/query');
+
+describe('rm3admin', function() {
+  this.timeout(10000); // This might take a bit of time
+  it('should add a permission to a role and roleinfo should check', function() {
+    childProcess.execSync('./bin/rm3admin permit root edit \\*');
+
+    var output = childProcess.execSync('./bin/rm3admin roleinfo root').toString();
+
+    output.should.match(/Permission: edit\tPath */);
+    output.should.match(/Permissions for role root:/);
+  });
+
+  it('should add a user', function(done) {
+    childProcess.execSync('./bin/rm3admin adduser ponies omgponies');
+
+    query.entityFromPath(db, entity.Entity, {}, {context: "ROOT"},
+      new sitepath('wh.users.ponies'), null, function(err, ent) {
+        should.not.exist(err);
+        ent.summary.title.should.equal('omgponies');
+        ent._proto.should.equal('user');
+        done();
+      });
+  });
+
+  it('should assign a user to a role', function() {
+    childProcess.execSync('./bin/rm3admin adduser kittens omgkittens');
+    childProcess.execSync('./bin/rm3admin assign kittens root');
+
+    var output = childProcess.execSync('./bin/rm3admin roleusers root').toString();
+
+    output.should.match(/wh.users.kittens/);
+    output.should.match(/Users in role root:/);
+  });
+
+});
---
+++ tests/cli/test-rm3load.js
@@ -0,0 +1,44 @@
+var Conf = require ('../../lib/conf');
+var should = require('should');
+var childProcess = require('child_process');
+
+var db = require('../../lib/db');
+var entity = require('../../lib/entity');
+var sitepath = require ('sitepath');
+var query = require('../../lib/query');
+
+describe('rm3load', function() {
+  this.timeout(8000); // This might take a bit of time
+  it('should load from a JSON file', function(done) {
+    childProcess.execSync('./bin/rm3load -f ./tests/page-fixtures/front.json');
+
+    query.entityFromPath(db, entity.Entity, {}, {context: "ROOT"},
+      new sitepath('wh'), null, function(err, ent) {
+        should.not.exist(err);
+        ent.summary.title.should.equal('Welcome to rm3');
+        done();
+      });
+  });
+
+  it('should load from a JSON file with overriden path', function(done) {
+    childProcess.execSync('./bin/rm3load -f ./tests/page-fixtures/front.json -p wh.rm3load.test1');
+
+    query.entityFromPath(db, entity.Entity, {}, {context: "ROOT"},
+      new sitepath('wh.rm3load.test1'), null, function(err, ent) {
+        should.not.exist(err);
+        ent.summary.title.should.equal('Welcome to rm3');
+        done();
+      });
+  });
+
+  it('should load from stdin with overriden path', function(done) {
+    childProcess.execSync('./bin/rm3load -p wh.rm3load.test2 < ./tests/page-fixtures/front.json');
+
+    query.entityFromPath(db, entity.Entity, {}, {context: "ROOT"},
+      new sitepath('wh.rm3load.test2'), null, function(err, ent) {
+        should.not.exist(err);
+        ent.summary.title.should.equal('Welcome to rm3');
+        done();
+      });
+  });
+});
