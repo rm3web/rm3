bb354de964d7fb4dd39a43b83e913af0aab0c41f
Ken Wronkiewicz <wh-github@wirewd.com>
2016-03-13 16:59:35 -0700

    Adding workflow tests.

---
+++ tests/db/test-workflow.js
@@ -0,0 +1,58 @@
+var uuid = require('node-uuid');
+var should = require('should');
+var runWorkflows = require('../../lib/runworkflows');
+var workflow = require('../../lib/workflow');
+
+require('mocha-steps');
+
+describe('workflows', function() {
+  this.timeout(8000); // This might take a bit of time
+  before(function(cb) {
+    runWorkflows(cb);
+  });
+
+  before(function(cb) {
+    workflow.loadConf(cb);
+  });
+
+  after(function() {
+    runWorkflows.killWorkflows();
+    workflow.gunBackend();
+  });
+
+  var jobUuid;
+
+  describe('works', function() {
+
+    step('#setupWorkflows', function(done) {
+      workflow.setupWorkflows(done);
+    });
+
+    step('#launchWorkflow', function(done) {
+      workflow.launchWorkflow("dummy", {}, function(err, jobid) {
+        if (err) {
+          should.fail(err);
+        }
+        jobUuid = jobid;
+        done(err);
+      });
+    });
+
+    step('#waitForWorkflow', function(done) {
+      workflow.waitForWorkflow(500, jobUuid, function(err, info) {
+        if (err) {
+          should.fail(err);
+        }
+        info.execution.should.equal('succeeded');
+        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        info.chain_results[0].result.should.equal('dummy task ran');
+        // jscs:enable
+        done(err);
+      });
+    });
+
+    step('#deleteWorkflows', function(done) {
+      workflow.deleteWorkflows(done);
+    });
+  });
+});
