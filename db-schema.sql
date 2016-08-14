69c47e639214430cef10af9c4ba58159bbe6a83f
Ken Wronkiewicz <wh-github@wirewd.com>
2016-08-13 19:58:11 -0700

    Add the ability to disable the login button
    Fixes #288

--- db-schema.sql
+++ db-schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE wh_siteconfig (
 INSERT INTO wh_siteconfig (site, path, data) VALUES
 		('default', 'site', '{"name": "WireWorld"}'),
     ('default', 'sitepath', '{"root": "wh", "urlroot": "http://127.0.0.1:4000/"}'),
-    ('default', 'login', '{"visible": "true"}');
+    ('default', 'login', '{"visible": true}');
 
 CREATE TABLE wh_entity (
 	PRIMARY KEY(path),
