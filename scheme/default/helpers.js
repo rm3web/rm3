f375ec06ae4875b5aaa85ba111e07c04dd70e8b7
Ken Wronkiewicz <wh-github@wirewd.com>
2016-09-05 13:41:04 -0700

    fix authorizations the rest of the way.

--- scheme/default/helpers.js
+++ scheme/default/helpers.js
@@ -164,6 +164,30 @@ exports = module.exports = function(dust, db, cache, query, reactDir) {
         }
     }
 
+    dust.helpers.requirePermissionOr = function(chunk, context, bodies, params) {
+        var path = context.resolve(params.path);
+        var permission = context.resolve(params.permission);
+        if (permission === 'edit' || permission === 'delete') {
+            permission = 'post.' + permission;
+        }
+
+        var permissionOr = context.resolve(params.permissionOr);
+        if (permissionOr === 'edit' || permissionOr === 'delete') {
+            permissionOr = 'post.' + permissionOr;
+        }
+
+        var user = context.get('user');
+        var permissions = context.get('permissions');
+
+        if (permission && (permissions.hasOwnProperty(permission) || permissions.hasOwnProperty(permissionOr))) {
+            return chunk.render(bodies.block, context);
+        } else {
+            if (bodies["else"]) {
+                return chunk.render(bodies["else"], context);
+            }
+        }
+    }
+
     dust.helpers.requireUser = function(chunk, context, bodies, params) {
         var user = context.get('user');
         if (user) {
