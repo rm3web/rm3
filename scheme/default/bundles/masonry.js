b8b263356ba9cfda2ee00c299ecb97ef9a2f7962
Ken Wronkiewicz <wh-github@wirewd.com>
2016-10-01 11:44:17 -0700

    Add imagesLoaded behavior.

--- scheme/default/bundles/masonry.js
+++ scheme/default/bundles/masonry.js
@@ -1,1 +1,10 @@
 var Masonry = require('masonry-layout');
+var imagesLoaded = require('imagesloaded');
+
+var msnry = new Masonry( '.masonry-grid', 
+  { "itemSelector": ".hentry-grid", "columnWidth": 200 });
+
+// selector string
+imagesLoaded( '#container', function() {
+  msnry.layout();
+});
