2ad96d5075689a17deaccbc10fb60c3d7dd0950c
Ken Wronkiewicz <wh-github@wirewd.com>
2020-01-05 12:03:46 -0800

    remove coveralls orb b'cause it doesn't seem to work.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -19,6 +19,10 @@ var winston = require('winston');
 
 winston.remove(winston.transports.Console);
 
+gulp.task('coveralls', shell.task([
+  'cat ./coverage/lcov.info |  ./node_modules/codecov.io/bin/codecov.io.js'
+]))
+
 gulp.task('travis', ['lint'])
 
 gulp.task('prepublish', function(callback) {
